28def5bd26946c73c5d80442300cf2b8a1afdac9
Grebenyuk Dmirtiy <sudo.grebenyuk.da@gmail.com>
2022-07-04 08:13:32 +0300

    Создал первый коммит

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// ПРОГРАММА ДЛЯ ВЫПОЛНЕНИЯ ПОСТАВЛЕННОЙ ЗАДАЧИ.
+
+string PrintArray(int[] mass)
+{
+    string line = String.Empty;
+    int arrayLine = mass.Length;
+    for (int i = 0; i < arrayLine; i++ )
+    {
+        line += $"{mass[i]} ";
+    }
+    return $"[{line}] ";
+}
+
