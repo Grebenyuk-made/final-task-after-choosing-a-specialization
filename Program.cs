eac0dc53eb85d74d99ddac2f655d0c0c5ad24cb0
Grebenyuk Dmirtiy <sudo.grebenyuk.da@gmail.com>
2022-07-04 16:51:49 +0300

    Создал второй коммит

--- Program.cs
+++ Program.cs
@@ -1,13 +1,38 @@
 ﻿// ПРОГРАММА ДЛЯ ВЫПОЛНЕНИЯ ПОСТАВЛЕННОЙ ЗАДАЧИ.
 
-string PrintArray(int[] mass)
-{
-    string line = String.Empty;
-    int arrayLine = mass.Length;
-    for (int i = 0; i < arrayLine; i++ )
+void PrintArray(string[] array)
+{   
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
     {
-        line += $"{mass[i]} ";
+        Console.Write($"{array[i]},");
     }
-    return $"[{line}] ";
+}
+ 
+void CreateFirstMass (int sizeMass, string[] mass)
+{
+    for (int i = 0; i < sizeMass; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}: ");
+        mass[i] = Console.ReadLine();
+    }
+
 }
 
+void СreatinSecondMass (string[] mass)
+{
+    string[] newMass = new string[sizeMass];
+    for (int i = 0; i < mass.Length; i++)
+    {
+        if (mass[i] <= 
+    }
+}
+
+
+
+Console.WriteLine("Введите размер массива: ");
+int sizeMass = int.Parse(Console.ReadLine() ?? "0");
+string[] mass = new string[sizeMass];
+CreateFirstMass(sizeMass, mass);
+Console.Write("Изначальный массив: ");
+PrintArray(mass);
